00645811b8e4b4a146ad7ba505bab5ed49f32fb8
Aaditi Dhadwal <aaditidhadwal@gmail.com>
2020-04-08 13:46:07 +0530

    bootstrap and styled component added.

---
+++ src/css/theme.js
@@ -0,0 +1,78 @@
+/* Basic Color */
+export const Theme = {
+  color: {
+    //   basic colors
+    white: '#ffffff',
+    purple: '#7e6990',
+    red: '#e43030',
+    pink: '#e83e8c',
+    orange: '#fd7e14',
+    yellow: '#ffee00',
+    green: '#00a002',
+    indigo: '#6610f2',
+    lightBlue: '#1472f5',
+    blue: '#264abb',
+    darkBlue: '#1b3892',
+    teal: '#55c0a9',
+    violet: '#8862e0',
+    black: '#000000',
+    transparent100: '#00000000',
+    transparent90: '#ffffff80',
+    transparent80: '#ffffff33',
+    bodyBgColor: '#f4f6f9',
+    baseColor: '#323232',
+    primaryColor: '#264abb',
+    secondaryColor: '#575757',
+    borderInput: '#ffffff80'
+  },
+  grayShade: {
+    gray900: '#111111',
+    gray800: '#222222',
+    gray700: '#333333',
+    gray600: '#444444',
+    gray500: '#575757',
+    gray400: '#666666',
+    gray300: '#777777',
+    gray200: '#888888',
+    gray100: '#959595',
+    gray90: '#f4f6f9',
+    gray50: '#f0eded'
+  },
+  font: {
+    size: {
+      /* Basic font size */
+      vSmall: '8px',
+      eSmall: '9px',
+      small: '12px',
+      base: '13px',
+      medium: '15px',
+      large: '17px',
+      extraLarge: '19px',
+      /* Title font size */
+      baseTitle: '18px',
+      primaryTitle: '35px',
+      secondaryTitle: '27px'
+    },
+    weight: {
+      /* Font Weight */
+      font300: '300',
+      font400: '400',
+      font500: '500',
+      font600: '600',
+      font700: '700',
+      font900: '900'
+    },
+    family: {
+      /* Font family */
+      baseFamily: 'Poppins-Light',
+      primary: 'Poppins-Regular',
+      semiBold: 'Poppins-Medium',
+      bold: 'Poppins - Bold'
+    }
+  },
+  lineHeight: {
+    lineHeight: 'auto'
+  }
+};
+
+export default Theme;
